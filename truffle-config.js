908fdc9df898596f82c80d15717a9215d56e60c3
Jaxcoder <codenamejason@outlook.com>
2020-02-21 11:22:54 -0500

    remote

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
@@ -7,6 +8,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    "ropsten-infura": {
+      provider: () => new HDWalletProvider("976b9d6972ea4ba3ba311f823564ed50", "https://ropsten.infura.io/v3/e8cc7c8e245b46b482873ce9382a542b"),
+      network_id: 3,
+      gas: 4700000
     },
   },
   contracts_directory: './src/contracts/',
